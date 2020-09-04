df2fdf922face2e610a24f9c6b2fe88c70049b4e
hitoshi <g.hitoshi.yokogawa@gmail.com>
2020-09-03 22:01:53 -0300

    GET & GET(id) feitos.
    Métodos de ListarTodos() e BuscarPorID realizados com sucesso.

---
+++ APIPet/APIPet/Controllers/RacaController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIPet.Domains;
+using APIPet.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APIPet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RacaController : ControllerBase
+    {
+        RacaRepository raca = new RacaRepository();
+        // GET: api/<RacaController>
+        [HttpGet]
+        public List<Raca> Get()
+        {
+            return raca.ListarTodos();
+        }
+
+        // GET api/<RacaController>/5
+        [HttpGet("{id}")]
+        public Raca Get(int id)
+        {
+            return raca.BuscarPorID(id);
+        }
+
+        // POST api/<RacaController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<RacaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RacaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- APIPet/APIPet/Repositories/RacaRepository.cs
+++ APIPet/APIPet/Repositories/RacaRepository.cs
@@ -1,7 +1,9 @@
-﻿using APIPet.Domains;
+﻿using APIPet.Context;
+using APIPet.Domains;
 using APIPet.Interface;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +11,10 @@ namespace APIPet.Repositories
 {
     public class RacaRepository : IRaca
     {
+
+        PetShopContext conexao = new PetShopContext();
+        SqlCommand cmd = new SqlCommand();
+
         public Raca Alterar(int id, Raca raca)
         {
             throw new NotImplementedException();
@@ -16,7 +22,24 @@ namespace APIPet.Repositories
 
         public Raca BuscarPorID(int id)
         {
-            throw new NotImplementedException();
+            cmd.Connection = conexao.Conectar();
+            cmd.CommandText = "SELECT * FROM Raca WHERE IdRaca = @id";
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            SqlDataReader dados = cmd.ExecuteReader();
+
+            Raca raca = new Raca();
+
+            while (dados.Read())
+            {
+                raca.IdRaca = Convert.ToInt32(dados.GetValue(0));
+                raca.Descricao = dados.GetValue(1).ToString();
+                raca.IdTipoDePet = Convert.ToInt32(dados.GetValue(2));
+            }
+
+            conexao.Desconectar();
+            return raca;
         }
 
         public Raca Cadastrar(Raca raca)
@@ -31,7 +54,29 @@ namespace APIPet.Repositories
 
         public List<Raca> ListarTodos()
         {
-            throw new NotImplementedException();
+            cmd.Connection = conexao.Conectar();
+            cmd.CommandText = "SELECT * FROM Raca";
+
+            SqlDataReader dados = cmd.ExecuteReader();
+
+            List<Raca> raca = new List<Raca>();
+
+            while (dados.Read())
+            {
+
+                raca.Add(
+                    new Raca()
+                    {
+                        IdRaca = Convert.ToInt32(dados.GetValue(0)),
+                        Descricao = dados.GetValue(1).ToString(),
+                        IdTipoDePet = Convert.ToInt32(dados.GetValue(2))
+                    }
+
+                    );
+            }
+
+            conexao.Desconectar();
+            return raca;
         }
     }
 }
